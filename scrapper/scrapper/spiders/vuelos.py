1813d75e0b67d547b16ab3c734cec28006ad6bcc
edxr <edxavier05@gmail.com>
2023-10-27 09:05:18 -0600

    configure

--- scrapper/scrapper/spiders/vuelos.py
+++ scrapper/scrapper/spiders/vuelos.py
@@ -6,5 +6,17 @@ class VuelosSpider(scrapy.Spider):
     allowed_domains = ["eaai.com.ni"]
     start_urls = ["https://www.eaai.com.ni/fids/vuelos_dias_fids.php?option=A"]
 
-    def parse(self, response):
+    def start_requests(self):
+        yield scrapy.Request(
+            url='https://www.eaai.com.ni/fids/vuelos_dias_fids.php?option=A',
+            meta={'playwright': True},
+            callback=self.parse, errback=self.errback_close_page)
+
+    async def parse(self, response):
         print(response.text)
+
+    async def errback_close_page(self, response):
+        page = response.request
+        print(page)
+        print(response)
+
